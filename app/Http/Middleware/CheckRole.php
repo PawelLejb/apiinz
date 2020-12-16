3a42587e0067118e03ef41414c770aa2b3d7a6b7
PawelLejb <51033853+PawelLejb@users.noreply.github.com>
2020-12-16 21:54:15 +0100

    Update CheckRole.php

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -7,15 +7,16 @@ use Illuminate\Http\Request;
 
 class CheckRole
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
     public function handle(Request $request, Closure $next)
     {
+
+        $validator = Validator::make($request->all(), [
+            'VerySecureKey' => 'required']);
+        if ($validator->fails()) {
+            return response()->json([
+                "message" => "Nie masz dostępu"
+            ], 400);
+        }
         return $next($request);
     }
 }
