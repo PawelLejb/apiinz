e843c96e5bb2cf4f90964c37297c62fb59a7f459
PawelLejb <51033853+PawelLejb@users.noreply.github.com>
2020-12-26 01:58:36 +0100

    Update passwords.php

--- resources/lang/en/passwords.php
+++ resources/lang/en/passwords.php
@@ -12,11 +12,17 @@ return [
     | has failed, such as for an invalid token or invalid new password.
     |
     */
-
+    /*
     'reset' => 'Your password has been reset!',
     'sent' => 'We have emailed your password reset link!',
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that email address.",
-
+    */
+     'reset' => 'Hasło zresetowane!',
+    'sent' => 'Wysłaliśmy link do zmiany hasła na maila!',
+    'throttled' => 'Proszę poczekać!.',
+    'token' => 'Błędny token zmiany hasła.',
+    'user' => "Nie możemy znaleźć użytkownika o podanym adresie email.",
+    
 ];
