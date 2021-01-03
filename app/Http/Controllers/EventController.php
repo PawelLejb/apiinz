a2c78c432259d1ed692e50c9a3804a9f8fda2d4e
PawelLejb <51033853+PawelLejb@users.noreply.github.com>
2021-01-03 18:47:36 +0100

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -14,10 +14,10 @@ class EventController extends Controller
         $id=$user->id;
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|between:2,100',
-            'place' => 'string|between:2,100',
-            'description' => 'string|min:1',
+            'place' => 'string',
+            'description' => 'string',
             'colour'=> 'required',
-            'category'=> 'string|min:2',
+            'category'=> 'string',
             'Users_idUser'=>'',
 
 
