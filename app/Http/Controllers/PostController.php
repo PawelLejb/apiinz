57ed1ef4efd104b903ec152bc9d3d4795f14320f
PawelLejb <51033853+PawelLejb@users.noreply.github.com>
2021-01-15 23:36:29 +0100

    Update PostController.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -125,7 +125,7 @@ class PostController extends Controller
 
             $post = DB::table('posts')
                  ->join('comments','posts.id','=','comments.Posts_idPost')
-                ->select('posts.id','posts.title','posts.post','posts.author','posts.authorId','posts.updated_at','posts.created_at','posts.Groups_idGroup','count(comments.Posts_idPost) as commentCount')
+                ->select('posts.id','posts.title','posts.post','posts.author','posts.authorId','posts.updated_at','posts.created_at','posts.Groups_idGroup',count('comments.Posts_idPost) as commentCount'))
                 ->groupBy('Posts_idPost')
                 ->where('posts.id','=',$postId)
               
